63102f84fbb3b89850e23a6d98595dd227a22d9a
Phu Tran <phu13325@gmail.com>
2021-03-17 08:38:22 +0700

    Update Service

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { LoginService } from 'src/app/services/login.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  public data : {username : string;password: string;}
+  // public data : { username: string;password: string;} = {username:'', password: ''}
+  constructor(private service : LoginService) {
+    this.data = {username:'', password: ''}
+   }
+  ngOnInit(): void {
+  }
+  // async getUser() {
+  //   let result = await this.service.userLogged(this.data);
+  //   console.log(result);
+  //  }
+}
---
+++ src/app/services/login.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+    providedIn: 'root',
+  })
+
+export class LoginService{
+    constructor(private http: HttpClient){}
+
+    userLogged(data : any){
+      return this.http.post("https://localhost:44346/api/auth/login",data).toPromise();
+  }
+}
